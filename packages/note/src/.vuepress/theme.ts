58e3c227195c9869eea3588598b9b6dc2e7d0af7
liuruizhou <liuruizhou@bilibili.com>
2023-02-20 00:42:21 +0800

    add theme

--- packages/note/src/.vuepress/theme.ts
+++ packages/note/src/.vuepress/theme.ts
@@ -20,6 +20,15 @@ export default hopeTheme({
 
   docsBranch: "master",
 
+  themeColor: {
+    blue: "#2196f3",
+    red: "#f26d6d",
+    green: "#3eaf7c",
+    orange: "#fb9b5f",
+  },
+
+  fullscreen: true,
+
   locales: {
     "/": {
       // navbar
