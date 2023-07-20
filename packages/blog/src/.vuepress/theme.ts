d7b7e3eec3c6d858015558afd671502e83240247
liuruizhou <liuruizhou@bilibili.com>
2023-07-20 17:28:04 +0800

    add

--- packages/blog/src/.vuepress/theme.ts
+++ packages/blog/src/.vuepress/theme.ts
@@ -98,7 +98,6 @@ export default hopeTheme({
     mdEnhance: {
       align: true,
       attrs: true,
-      card: true,
       chart: true,
       codetabs: true,
       demo: true,
@@ -107,28 +106,15 @@ export default hopeTheme({
       flowchart: true,
       gfm: true,
       imgLazyload: true,
-      imgMark: true,
       imgSize: true,
-      // include: {
-      //   resolvePath: (file, cwd) => {
-      //     if (file.startsWith("@echarts"))
-      //       return file.replace(
-      //         "@echarts",
-      //         path.resolve(__dirname, "../echarts")
-      //       );
-
-      //     return file;
-      //   },
-      // },
-      mathjax: true,
+      include: true,
+      katex: true,
       mark: true,
       mermaid: true,
       playground: {
         presets: ["ts", "vue"],
       },
-      presentation: {
-        plugins: ["highlight", "math", "search", "notes", "zoom"],
-      },
+      presentation: ["highlight", "math", "search", "notes", "zoom"],
       stylize: [
         {
           matcher: "Recommended",
