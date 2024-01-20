64ee9d984b6bcf27527ce30e4df61e4aeaee8ee4
zhou <ruizhou@163.com>
2024-01-20 15:40:16 +0800

    fix

--- packages/blog/src/.vuepress/config.ts
+++ packages/blog/src/.vuepress/config.ts
@@ -1,5 +1,5 @@
 import { defineUserConfig } from "@vuepress/cli";
-// import { searchProPlugin } from "vuepress-plugin-search-pro";
+import { searchProPlugin } from "vuepress-plugin-search-pro";
 import theme from "./theme.js";
 
 export default defineUserConfig({
@@ -23,21 +23,21 @@ export default defineUserConfig({
 
   shouldPrefetch: false,
 
-  // plugins: [
-  //   searchProPlugin({
-  //     // 索引全部内容
-  //     indexContent: true,
-  //     // 为分类和标签添加索引
-  //     customFields: [
-  //       {
-  //         getter: (page) => page.frontmatter.category,
-  //         formatter: "分类：$content",
-  //       },
-  //       {
-  //         getter: (page) => page.frontmatter.tag,
-  //         formatter: "标签：$content",
-  //       },
-  //     ],
-  //   }),
-  // ],
+  plugins: [
+    searchProPlugin({
+      // 索引全部内容
+      indexContent: true,
+      // 为分类和标签添加索引
+      customFields: [
+        {
+          getter: (page) => page.frontmatter.category,
+          formatter: "分类：$content",
+        },
+        {
+          getter: (page) => page.frontmatter.tag,
+          formatter: "标签：$content",
+        },
+      ],
+    }),
+  ],
 });
--- packages/blog/src/.vuepress/theme.ts
+++ packages/blog/src/.vuepress/theme.ts
@@ -72,7 +72,6 @@ export default hopeTheme({
         "Replit",
         "SiteInfo",
         "StackBlitz",
-        "VidStack",
       ],
     },
 
@@ -131,22 +130,6 @@ export default hopeTheme({
       tabs: true,
       vPre: true,
       vuePlayground: true,
-    },
-
-    searchPro: {
-      // 索引全部内容
-      indexContent: true,
-      // 为分类和标签添加索引
-      customFields: [
-        {
-          getter: (page) => page.frontmatter.category,
-          formatter: "分类：$content",
-        },
-        {
-          getter: (page) => page.frontmatter.tag,
-          formatter: "标签：$content",
-        },
-      ],
     },
   },
 });
