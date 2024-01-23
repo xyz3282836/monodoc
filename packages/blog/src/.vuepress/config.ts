ec278e78542aa5157ffde264d1d5fbaabc755b9e
liuruizhou <liuruizhou@bilibili.com>
2024-01-23 19:30:36 +0800

    update doc

--- packages/blog/src/.vuepress/config.ts
+++ packages/blog/src/.vuepress/config.ts
@@ -1,5 +1,4 @@
 import { defineUserConfig } from "@vuepress/cli";
-import { searchProPlugin } from "vuepress-plugin-search-pro";
 import theme from "./theme.js";
 
 export default defineUserConfig({
@@ -23,21 +22,4 @@ export default defineUserConfig({
 
   shouldPrefetch: false,
 
-  plugins: [
-    searchProPlugin({
-      // 索引全部内容
-      indexContent: true,
-      // 为分类和标签添加索引
-      customFields: [
-        {
-          getter: (page) => page.frontmatter.category,
-          formatter: "分类：$content",
-        },
-        {
-          getter: (page) => page.frontmatter.tag,
-          formatter: "标签：$content",
-        },
-      ],
-    }),
-  ],
 });
